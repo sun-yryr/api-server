67177935e63a17739181673ce2e285587c24ffb7
sinbata <batabata4096@gmail.com>
2019-01-14 23:21:34 +0900

    .

--- routes/record.js
+++ routes/record.js
@@ -1,5 +1,6 @@
 /* パッケージ読み込み */
 var express = require('express');
+var shiori = require('./shiwori');
 var router = express.Router();
 
 const BookData = JSON.stringify(require("../bookdata.json"));
@@ -24,17 +25,18 @@ router.post('/', async function(req, res, next) {
   
     var user_id = req.body['user_id'];
     var book_id = req.body['book_id'];
-    var user_name = req.body['user_name'];;
-    var star = req.body['star'];;
+    var user_name = req.body['user_name'];
+    var star = req.body['star'];
     var impression = req.body['impression'];
-    var readtime = req.body['readtime'];;
+    var readtime = req.body['readtime'];
     var readspeed = req.body['readspeed'];
     var created_date = req.body['created_date'];
 
     /*最終段を持ってくる */
 
     /*データの登録 */
-    let sql = 'INSERT INTO records (user_id,book_id,user_name,star,impression,readtime,readspeed,creted_date) VALUES (user_id,book_id,user_name,star,impression,readtime,readspeed,creted_date)';
+    var db_res =await shiori.dbAccess('INSERT INTO records (user_id,book_id,user_name,star,impression,readtime,readspeed,creted_date) VALUES (user_id,book_id,user_name,star,impression,readtime,readspeed,creted_date)');
+    res.status(200);
 });
 
 
