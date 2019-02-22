83eac309f6bd7fe4f709c5695452ef977ba28912
sinbata <batabata4096@gmail.com>
2019-02-22 16:36:16 +0900

    current_book

--- routes/device.js
+++ routes/device.js
@@ -6,15 +6,16 @@ var router = express.Router();
 
 /* urlの受け口を実装する */
 /* root(/) is /shiwori/device */
-router.post('/insert', async function(req, res, next) {
+router.post('/insert', shiwori.check_signature, async function(req, res, next) {
   console.log("device.post...");
     var user_id = req.body['user_id'];
-    var db_res = await shiwori.dbAccess("select current_bookid from users where user_id='"+user_id+"'");
-    var book_id = db_res[0].current_bookid;
-    if(book_id!=req.body['current_bookid']) {
-      res.status(400).json({"message": "invalid this current_bookid."});
+    var db_res = await shiwori.dbAccess("select current_book_id from users where user_id='"+user_id+"'");
+    if(!db_res[0]){
+      res.status(500).end();
+      return;
     }
-    var timestamp = req.body['timestamp'];
+    var book_id = db_res[0].current_book_id;
+    var timestamp = shiwori.getNowTime();
     var page_num = req.body['page_num'];
     var readtime = req.body['readtime'];
     var readspeed = req.body['readspeed'];
@@ -22,22 +23,22 @@ router.post('/insert', async function(req, res, next) {
     /*最終段を持ってくる */
 
     /*データの登録 */
-    var sql = util.format('INSERT INTO records (user_id,book_id,timestamp,page_num,readtime,readspeed) VALUES ("%s","%s","%s",%d,"%d","%d")',user_id,book_id,timestamp,readtime,readspeed);
+    var sql = util.format('INSERT INTO device (user_id,book_id,timestamp,page_num,readtime,readspeed) VALUES ("%s","%s","%s",%d,"%d","%d")',user_id,book_id,timestamp,page_num,readtime,readspeed);
     db_res =await shiwori.dbAccess(sql).catch((err)=>{
       console.log(err);
       res.status(500).json({"message":err});
     });
-    res.status(200).json({"record_id": db_res.insertId});
+    res.status(200).end();
 });
 
 /* データのList取得 */
-router.get('/get', shiwori.check_signature, async function(req, res, next) {
+router.get('/get', async function(req, res, next) {
   console.log("device.list...");
-  const db_data = await shiwori.dbAccess('SELECT * FROM device WHERE user_id="' + req.query.user_id + '"')
+  const db_res = await shiwori.dbAccess('SELECT * FROM device WHERE user_id="' + req.query.user_id + '"')
     .catch((err) => {
       res.status(500).json({"message": err});
   });
-  Promise.all(db_data.map(async function(item) {
+  Promise.all(db_data.map(async function(item) {//
     var tmp = {
       "user_id": item.user_id,
       "book_id": item.book_id,
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var shiwori = require('./shiwori');
-
+var util = require('util');
 /* グローバル変数 */
 
 /* 関数 */
@@ -26,6 +26,32 @@ router.get('/', function(req, res, next) {
     "created_date": db_res[0].created_date
   }
   res.status(200).json(tmp);
+});
+
+router.post('/current_book', shiwori.check_signature,async function(req, res, next) {
+  
+  var user_id = req.body['user_id'];
+  var sql = util.format('SELECT * FROM users WHERE user_id="%s"',user_id);
+  var db_res  = await shiwori.dbAccess(sql)
+  if(db_res.length != 1){
+    res.status(500).json({
+      "message": "this userid is not found"
+    })
+    return;
+  }
+  var current_book_id = req.body['current_book_id'];
+
+  /*最終段を持ってくる */
+
+  /*データの登録 */
+  // UPDATE テーブル名 SET カラム名=`値`[, カラム名=`値`, ... ] WHERE 条件式;
+
+  var sql = util.format('UPDATE users SET current_book_id="%s" WHERE user_id="%s"',current_book_id,user_id);
+  db_res =await shiwori.dbAccess(sql).catch((err)=>{
+    console.log(err);
+    res.status(500).json({"message":err});
+  });
+  res.status(200).end();
 });
 
 module.exports = router;
